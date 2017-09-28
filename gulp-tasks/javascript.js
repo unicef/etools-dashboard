2c2d7e3094777a527748fcc403b148f70bfde76f
Marko Bilal <marko.barca10@gmail.com>
2017-09-28 12:08:54 -0400

    Revert "Add babel transpile to gulp tasks (enables full es6)"

--- gulp-tasks/javascript.js
+++ gulp-tasks/javascript.js
@@ -3,26 +3,25 @@
 
 'use strict';
 
-const harmony = require('uglify-js-harmony');
+const uglifyHarmony = require('uglify-js-harmony');
 const minifier = require('gulp-uglify/minifier');
 const jshint = require('gulp-jshint');
 const jshintStylish = require('jshint-stylish');
 const jscs = require('gulp-jscs');
 const jscsStylish = require('gulp-jscs-stylish');
 const lazypipe = require('lazypipe'); // Lazy pipe creates a reusable pipe stream
-const $ = require('gulp-load-plugins')();
-const gulpif = require('gulp-if');
+
 // Minify Javascript
 function minify() {
-  const uglifyOptions = {
+  var uglifyOptions = {
     preserveComments: false
     // options
   };
-  return minifier(uglifyOptions, harmony);
+  return minifier(uglifyOptions, uglifyHarmony);
 }
 
 // Lint Javascript
-const lint = lazypipe()
+var lint = lazypipe()
   .pipe(jshint)
   .pipe(jscs)
   .pipe(jscsStylish.combineWithHintResults)
@@ -31,15 +30,7 @@ const lint = lazypipe()
   //.pipe(jshint.reporter, 'fail');
   .pipe(jshint.reporter);
 
-const babelify = lazypipe()
-  .pipe(()=> ($.if('*.html', $.crisper({scriptInHead:false}))))
-  .pipe(()=> ($.if('*.js', $.babel({
-    presets: ['es2017'],
-    plugins: ['transform-object-rest-spread']
-  }))));
-
 module.exports = {
-  babelify: babelify,
   minify: minify,
   lint: lint
 };
