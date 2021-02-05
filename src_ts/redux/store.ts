2c9671fd017ff94c128ffe71084c7ffac972922a
Adriana Trif <trif.corina.adriana@gmail.com>
2021-02-05 22:17:04 +0200

    clean up

--- src_ts/redux/store.ts
+++ src_ts/redux/store.ts
@@ -18,11 +18,6 @@ const devCompose: <Ext0, StateExt0>(
 ) => StoreEnhancer<Ext0, StateExt0> = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
-// Initializes the Redux store with a lazyReducerEnhancer (so that you can
-// lazily add reducers after the store has been created) and redux-thunk (so
-// that you can dispatch async actions). See the "Redux and state management"
-// section of the wiki for more details:
-// https://github.com/Polymer/pwa-starter-kit/wiki/4.-Redux-and-state-management
 export const store = createStore(staticDataReducer,
   devCompose(applyMiddleware(thunk as any)));
 
