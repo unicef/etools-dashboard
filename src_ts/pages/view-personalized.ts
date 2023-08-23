46bcbc0ea8e7d25c23f618c7c4bd59adac59419d
Trif Adriana <trif.corina.adriana@gmail.com>
2023-08-23 15:15:54 +0300

    My dash prod link update

--- src_ts/pages/view-personalized.ts
+++ src_ts/pages/view-personalized.ts
@@ -50,7 +50,13 @@ export class ViewPersonalized extends PolymerElement {
     if (this.environment?.includes('DEV')) {
       embedSource = personalizedDev + `&filter=user/email eq '${email}'`;
     } else {
-      embedSource = personalizedProd + `&filter=user/email eq '${email}'`;
+      embedSource =
+        personalizedProd +
+        +`&$filter=interventions_focalpoints/unicef_focal_point_email eq '${email}'` +
+        ` and actionpointsfor/assigned_to_email eq '${email}'` +
+        ` and actionpointsby/assigned_by_email eq '${email}'` +
+        ` and tripsby/supervisor_email eq '${email}'` +
+        ` and tripsfor/traveler_email eq '${email}'`;
     }
 
     this.set('embedSource', embedSource);
